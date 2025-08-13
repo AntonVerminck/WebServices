285c0110c9a4676a49e55ce179f5bee0628f9e34
Anton <anton.verminck@student.hogent.be>
2025-08-13 17:32:55 +0200

    migrations error voor online

--- src/data/migrations/20250813152439_init/migration.sql
+++ src/data/migrations/20250813152439_init/migration.sql
@@ -1,14 +1,13 @@
 -- CreateTable
-CREATE TABLE `Screening` (
+CREATE TABLE `screening` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     `film_id` INTEGER UNSIGNED NOT NULL,
     `naam` VARCHAR(255) NOT NULL,
     `huisnummer` TINYINT UNSIGNED NOT NULL,
     `postcode` INTEGER UNSIGNED NOT NULL,
     `straat` VARCHAR(255) NOT NULL,
-    `datetime` DATETIME(3) NOT NULL,
+    `datum` DATETIME(0) NOT NULL,
 
-    UNIQUE INDEX `idx_name_unique`(`naam`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -26,7 +25,9 @@ CREATE TABLE `user_film` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id` INTEGER UNSIGNED NOT NULL,
     `film_id` INTEGER UNSIGNED NOT NULL,
-    `rating` INTEGER NULL,
+    `rating` INTEGER UNSIGNED NOT NULL,
+    `review_titel` VARCHAR(255) NOT NULL,
+    `review_content` VARCHAR(255) NOT NULL,
 
     UNIQUE INDEX `user_film_user_id_film_id_key`(`user_id`, `film_id`),
     PRIMARY KEY (`id`)
@@ -35,19 +36,21 @@ CREATE TABLE `user_film` (
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    `naam` VARCHAR(255) NOT NULL,
+    `voornaam` VARCHAR(255) NOT NULL,
+    `achternaam` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `password_hash` VARCHAR(255) NOT NULL,
+    `roles` JSON NOT NULL,
 
     UNIQUE INDEX `idx_user_email_unique`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Screening` ADD CONSTRAINT `fk_screening_film` FOREIGN KEY (`film_id`) REFERENCES `films`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `screening` ADD CONSTRAINT `fk_screening_film` FOREIGN KEY (`film_id`) REFERENCES `films`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `user_film` ADD CONSTRAINT `fk_userfilm_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `user_film` ADD CONSTRAINT `fk_userfilm_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `user_film` ADD CONSTRAINT `fk_userfilm_film` FOREIGN KEY (`film_id`) REFERENCES `films`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `user_film` ADD CONSTRAINT `fk_userfilm_film` FOREIGN KEY (`film_id`) REFERENCES `films`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;

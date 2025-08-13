b28ba7b63e514d319d4380583c264be5a07a6898
Anton <anton.verminck@student.hogent.be>
2025-08-13 18:40:32 +0200

    changes

--- src/service/health.ts
+++ src/service/health.ts
@@ -1,9 +1,7 @@
 import config from 'config';
-const packageJson = require('../../../package.json');
-
+import  packageJson from '../../package.json';
 
 export const ping = () => ({ pong: true });
-
 
 export const getVersion = () => ({
   env: config.get<string>('env'),
